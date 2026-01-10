670d3849236a2efe917dc043aa2c9b111d6da06f
enklht <enklht@gmail.com>
2026-01-10 17:34:21 +0900

    refactor: :recycle: improve SPH simulation with proper kernel functions and physics parameters

--- src/kernel.rs
+++ src/kernel.rs
@@ -1,15 +1,15 @@
-use super::SIGMA;
+use super::H;
 use macroquad::prelude::*;
 use std::f32::consts::PI;
 
 #[allow(unused)]
 pub fn poly6(x: Vec2) -> f32 {
-    const SIGMA2: f32 = SIGMA * SIGMA;
-    const SIGMA8: f32 = SIGMA2 * SIGMA2 * SIGMA2 * SIGMA2;
+    const H2: f32 = H * H;
+    const H8: f32 = H2 * H2 * H2 * H2;
 
     let r2 = x.length_squared();
-    if r2 < SIGMA.powi(2) {
-        4. / (PI * SIGMA8) * (SIGMA2 - r2).powi(3)
+    if r2 < H.powi(2) {
+        4. / (PI * H8) * (H2 - r2).powi(3)
     } else {
         0.
     }
@@ -17,24 +17,37 @@ pub fn poly6(x: Vec2) -> f32 {
 
 #[allow(unused)]
 pub fn grad_poly6(x: Vec2) -> Vec2 {
-    const SIGMA2: f32 = SIGMA * SIGMA;
-    const SIGMA8: f32 = SIGMA2 * SIGMA2 * SIGMA2 * SIGMA2;
+    const H2: f32 = H * H;
+    const H8: f32 = H2 * H2 * H2 * H2;
 
     let r2 = x.length_squared();
-    if r2 < SIGMA.powi(2) {
-        -24. / (PI * SIGMA8) * (SIGMA2 - r2).powi(2) * x
+    if r2 < H.powi(2) {
+        -24. / (PI * H8) * (H2 - r2).powi(2) * x
     } else {
         Vec2::ZERO
     }
 }
 
 #[allow(unused)]
+pub fn lap_poly6(x: Vec2) -> f32 {
+    const H2: f32 = H * H;
+    const H8: f32 = H2 * H2 * H2 * H2;
+
+    let r2 = x.length_squared();
+    if r2 < H.powi(2) {
+        48. / (PI * H8) * (H2 - r2).powi(2) * (3. * r2 - H2)
+    } else {
+        0.
+    }
+}
+
+#[allow(unused)]
 pub fn spiky(x: Vec2) -> f32 {
-    const SIGMA5: f32 = SIGMA * SIGMA * SIGMA * SIGMA * SIGMA;
+    const H5: f32 = H * H * H * H * H;
 
     let r = x.length();
-    if r < SIGMA {
-        10. / (PI * SIGMA5) * (SIGMA - r).powi(3)
+    if r < H {
+        10. / (PI * H5) * (H - r).powi(3)
     } else {
         0.
     }
@@ -42,12 +55,24 @@ pub fn spiky(x: Vec2) -> f32 {
 
 #[allow(unused)]
 pub fn grad_spiky(x: Vec2) -> Vec2 {
-    const SIGMA5: f32 = SIGMA * SIGMA * SIGMA * SIGMA * SIGMA;
+    const H5: f32 = H * H * H * H * H;
 
     let r = x.length();
-    if 0. < r && r < SIGMA {
-        -30. / (PI * SIGMA5) * (SIGMA - r).powi(2) * (x / r)
+    if 0. < r && r < H {
+        -30. / (PI * H5) * (H - r).powi(2) * (x / r)
     } else {
         Vec2::ZERO
+    }
+}
+
+#[allow(unused)]
+pub fn lap_visc(x: Vec2) -> f32 {
+    const H5: f32 = H * H * H * H * H;
+
+    let r = x.length();
+    if r < H {
+        20. / (3. * PI * H5) * (H - r)
+    } else {
+        0.
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -8,26 +8,25 @@ use rayon::prelude::*;
 use std::collections::HashMap;
 
 const RADIUS: f32 = 5.;
-const STEPS_PER_FRAME: usize = 10;
+const STEPS_PER_FRAME: usize = 1;
 
-const SIGMA: f32 = 20.;
+const G_SCALE: f32 = 15.;
+// kernel size
+const H: f32 = 20.;
+// mass
+const M: f32 = 20.;
+// stiffness
+const K: f32 = 1.5e5;
+// viscosity
+const MU: f32 = 1e2;
 
-const GAMMA: i32 = 7;
-const B: f32 = 1.5;
-
-const M: f32 = 1.;
-
-const EPS: f32 = 0.1;
-
-const DT: f32 = 1. / 60.;
+const DT: f32 = 1. / 30.;
 
 type SpatialHashGrid = HashMap<(usize, usize), Vec<usize>>;
 
+#[inline]
 fn cell_coord(pos: &Vec2) -> (usize, usize) {
-    (
-        (pos.x / SIGMA).round() as usize,
-        (pos.y / SIGMA).round() as usize,
-    )
+    ((pos.x / H).round() as usize, (pos.y / H).round() as usize)
 }
 
 #[derive(Default)]
@@ -37,18 +36,17 @@ struct Scene {
     rho0: f32,
     pos: Vec<Vec2>,
     vel: Vec<Vec2>,
-    acc: Vec<Vec2>,
+    force: Vec<Vec2>,
     rho: Vec<f32>,
     p: Vec<f32>,
 }
 
 impl Scene {
     fn new(w: f32, h: f32) -> Scene {
-        let space = 2.0 * RADIUS * 1.5;
-        let pad = RADIUS * 2.0;
+        let space = 2.0 * RADIUS;
 
-        let cols = ((w - 2.0 * pad) / space).floor() as usize;
-        let rows = ((h - 2.0 * pad) / 3.0 / space).floor() as usize;
+        let cols = ((w - 2.0 * space) / space).floor() as usize;
+        let rows = ((h - 2.0 * space) / 3.0 / space).floor() as usize;
 
         let num_particles = cols * rows;
 
@@ -56,8 +54,8 @@ impl Scene {
 
         for row in 0..rows {
             for col in 0..cols {
-                let mut x = pad + col as f32 * space;
-                let y = pad + row as f32 * space;
+                let mut x = space + col as f32 * space;
+                let y = space + row as f32 * space;
 
                 // stagger every other row
                 if row % 2 == 1 {
@@ -74,8 +72,8 @@ impl Scene {
             rho0: 0.,
             pos,
             vel: vec![Vec2::ZERO; num_particles],
-            acc: vec![Vec2::ZERO; num_particles],
-            rho: vec![0.; num_particles],
+            force: vec![Vec2::ZERO; num_particles],
+            rho: vec![1.; num_particles],
             p: vec![0.; num_particles],
         }
     }
@@ -96,8 +94,8 @@ impl Scene {
 
             let cell = cell_coord(&self.pos[i]);
 
-            for x in cell.0.saturating_sub(1)..=cell.0 + 1 {
-                for y in cell.1.saturating_sub(1)..=cell.1 + 1 {
+            for x in cell.0.saturating_sub(1)..=cell.0.saturating_add(1) {
+                for y in cell.1.saturating_sub(1)..=cell.1.saturating_add(1) {
                     let Some(neighbours) = self.grid.get(&(x, y)) else {
                         continue;
                     };
@@ -106,6 +104,21 @@ impl Scene {
                         *rho += M * poly6(self.pos[i] - self.pos[j]);
                     }
                 }
+            }
+
+            if *rho == 0. {
+                println!("{} at {:?}", i, cell);
+                for x in cell.0.saturating_sub(1)..=cell.0.saturating_add(1) {
+                    for y in cell.1.saturating_sub(1)..=cell.1.saturating_add(1) {
+                        let Some(neighbours) = self.grid.get(&(x, y)) else {
+                            continue;
+                        };
+
+                        println!("{}, {}: {:?}", x, y, neighbours);
+                    }
+                }
+
+                panic!()
             }
         });
 
@@ -116,31 +129,28 @@ impl Scene {
 
     fn update_pressure(&mut self) {
         for i in 0..self.num_particles {
-            self.p[i] = B * ((self.rho[i] / self.rho0).powi(GAMMA) - 1.);
+            self.p[i] = K * (self.rho[i] - self.rho0);
         }
     }
 
-    fn update_acceleration(&mut self) {
-        let base_acc = get_acceleration();
+    fn update_force(&mut self) {
+        let base_force = get_acceleration() * G_SCALE;
 
-        self.acc.par_iter_mut().enumerate().for_each(|(i, acc)| {
-            *acc = base_acc;
-
+        self.force.par_iter_mut().enumerate().for_each(|(i, f)| {
+            *f = base_force;
             let cell = cell_coord(&self.pos[i]);
 
-            for x in cell.0.saturating_sub(1)..=cell.0 + 1 {
-                for y in cell.1.saturating_sub(1)..=cell.1 + 1 {
+            for x in cell.0.saturating_sub(1)..=cell.0.saturating_add(1) {
+                for y in cell.1.saturating_sub(1)..=cell.1.saturating_add(1) {
                     let Some(neighbours) = self.grid.get(&(x, y)) else {
                         continue;
                     };
 
                     for &j in neighbours {
-                        if i != j {
-                            *acc += -M
-                                * (self.p[i] / self.rho[i].powi(2)
-                                    + self.p[j] / self.rho[j].powi(2))
-                                * grad_spiky(self.pos[i] - self.pos[j])
-                        }
+                        *f += -M * (self.p[i] + self.p[j]) / (2. * self.rho[j])
+                            * grad_spiky(self.pos[i] - self.pos[j]);
+                        *f += MU * M * (self.vel[j] - self.vel[i]) / self.rho[j]
+                            * lap_visc(self.pos[i] - self.pos[j]);
                     }
                 }
             }
@@ -148,14 +158,16 @@ impl Scene {
     }
 
     fn apply_xsph(&mut self) {
+        const EPS: f32 = 0.5;
+
         let dv = (0..self.num_particles)
             .into_par_iter()
             .map(|i| {
                 let mut corr = Vec2::ZERO;
                 let cell = cell_coord(&self.pos[i]);
 
-                for x in cell.0.saturating_sub(1)..=cell.0 + 1 {
-                    for y in cell.1.saturating_sub(1)..=cell.1 + 1 {
+                for x in cell.0.saturating_sub(1)..=cell.0.saturating_add(1) {
+                    for y in cell.1.saturating_sub(1)..=cell.1.saturating_add(1) {
                         let Some(neighbours) = self.grid.get(&(x, y)) else {
                             continue;
                         };
@@ -208,7 +220,7 @@ impl Scene {
         clear_background(BACKGROUND_COLOR);
 
         for pos in &self.pos {
-            draw_circle(pos.x, pos.y, RADIUS * 2., WATER_COLOR);
+            draw_circle(pos.x, pos.y, RADIUS * 1.5, WATER_COLOR);
         }
     }
 }
@@ -225,21 +237,21 @@ async fn main() {
             let w = screen_width();
             let h = screen_height();
 
-            scene.update_grid();
-
             for i in 0..scene.num_particles {
-                scene.vel[i] += scene.acc[i] * DT / 2.;
+                scene.vel[i] += scene.force[i] / scene.rho[i] * DT / 2.;
                 scene.pos[i] += scene.vel[i] * DT;
             }
 
+            scene.update_grid();
+
             scene.update_density();
             scene.update_pressure();
-            scene.update_acceleration();
+            scene.update_force();
 
             scene.apply_xsph();
 
             for i in 0..scene.num_particles {
-                scene.vel[i] += scene.acc[i] * DT / 2.;
+                scene.vel[i] += scene.force[i] / scene.rho[i] * DT / 2.;
             }
 
             scene.handle_wall_collision(w, h);
